930befdea3ef9fc72a26129bfa138ae0aae70e8b
Thiago da Cunha Borges <thiagoborges@id.uff.br>
2017-02-16 12:14:08 -0200

    Reestilização do site. Criação do participe. Links para o grupo do facebook, grupo do telegram, pasta do google drive e grupo do slack.

--- _script/aulas.js
+++ _script/aulas.js
@@ -1,41 +1,47 @@
 //Quadro de disciplinas. Qualquer alteração de grade deverá ser feito apenas aqui.
 var disciplina = [];
-disciplina[0] = {periodo:"primeiro", nome:"Introdução a Informática", cod:"ead05001", aulas:13};
-disciplina[1] = {periodo:"primeiro", nome:"Projeto de Desenvolvimento de Algorismos", cod:"ead05005", aulas:15};
-disciplina[2] = {periodo:"primeiro", nome:"Construção de Páginas WEB", cod:"ead05002", aulas:20};
-disciplina[3] = {periodo:"segundo", nome:"Fundamentos de Algoritmo para Computação", cod:"ead05004", aulas:24};
-disciplina[4] = {periodo:"segundo", nome:"Organização de Computadores", cod:"ead05010", aulas:10};
-disciplina[5] = {periodo:"segundo", nome:"Fundamentos de Programação (Pascal)", cod:"ead05009", aulas:14};
-disciplina[6] = {periodo:"segundo", nome:"Fundamentos de Programação (Python)", cod:"ead05029", aulas:12};
-disciplina[7] = {periodo:"segundo", nome:"Álgebra Linear", cod:"ead05006", aulas:15};
-disciplina[8] = {periodo:"terceiro", nome:"Matemática para Computação", cod:"ead05008", aulas:9};
-disciplina[9] = {periodo:"terceiro", nome:"Física para Computação", cod:"ead05015", aulas:26};
-disciplina[10] = {periodo:"terceiro", nome:"Estrutura de Dados", cod:"ead05007", aulas:36};
-disciplina[11] = {periodo:"terceiro", nome:"Programação com Interfaces Gráficas (Pascal)", cod:"ead05013", aulas:17};
-disciplina[12] = {periodo:"terceiro", nome:"Programação com Interfaces Gráficas (Python)", cod:"ead05030", aulas:17};
-disciplina[13] = {periodo:"quarto", nome:"Probabilidade e Estatistica", cod:"ead05012", aulas:12};
-disciplina[14] = {periodo:"quarto", nome:"Modelagem de Informação", cod:"ead05011", aulas:23};
-disciplina[15] = {periodo:"quarto", nome:"Sistemas Operacionais", cod:"ead05016", aulas:12};
-disciplina[16] = {periodo:"quarto", nome:"Banco de Dados", cod:"ead05014", aulas:29};
-disciplina[17] = {periodo:"quarto", nome:"Programação Orientada a Objetos", cod:"ead05021", aulas:13};
-disciplina[18] = {periodo:"quinto", nome:"Engenharia de Software", cod:"ead05019", aulas:18};
-disciplina[19] = {periodo:"quinto", nome:"Redes de Computadores I", cod:"ead05020", aulas:14};
-disciplina[20] = {periodo:"quinto", nome:"Análise de Sistemas", cod:"ead05018", aulas:24};
-disciplina[21] = {periodo:"quinto", nome:"Programação de Aplicações WEB", cod:"ead05017", aulas:14};
-disciplina[22] = {periodo:"sexto", nome:"Empreendedorismo e Ética", cod:"ead05024", aulas:14};
-disciplina[23] = {periodo:"sexto", nome:"Redes de Computadores II", cod:"ead05025", aulas:23};
-disciplina[24] = {periodo:"sexto", nome:"Arquitetura de Sistemas", cod:"ead05022", aulas:21};
-disciplina[25] = {periodo:"sexto", nome:"Computação Gráfica", cod:"ead05023", aulas:19};
+disciplina[0] = {periodo:"primeiro", nome:"Introdução a Informática", cod:"ead05001", aulas:13, drive:"https://drive.google.com/drive/folders/0B09LpkQ_E4zTbzRwZjJmd2k0MkU?usp=sharing"};
+disciplina[1] = {periodo:"primeiro", nome:"Projeto de Desenvolvimento de Algorismos", cod:"ead05005", aulas:15, drive:"https://drive.google.com/drive/folders/0B09LpkQ_E4zTRDJ6dlpab0RQTmM?usp=sharing"};
+disciplina[2] = {periodo:"primeiro", nome:"Construção de Páginas WEB", cod:"ead05002", aulas:20, drive:"https://drive.google.com/drive/folders/0B09LpkQ_E4zTZC05X3gyN2l6Mnc?usp=sharing"};
+disciplina[3] = {periodo:"segundo", nome:"Fundamentos de Algoritmo para Computação", cod:"ead05004", aulas:24, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTeWtOaGxfVGdMdms"};
+disciplina[4] = {periodo:"segundo", nome:"Organização de Computadores", cod:"ead05010", aulas:10, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTSFUzd0pGYi1pc1k"};
+disciplina[5] = {periodo:"segundo", nome:"Fundamentos de Programação (Pascal)", cod:"ead05009", aulas:14, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTZ3JNY3pJWmY4amM"};
+disciplina[6] = {periodo:"segundo", nome:"Fundamentos de Programação (Python)", cod:"ead05029", aulas:12, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTZ3JNY3pJWmY4amM"};
+disciplina[7] = {periodo:"segundo", nome:"Álgebra Linear", cod:"ead05006", aulas:15, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTU1dqMXVBYlNlMGM"};
+disciplina[8] = {periodo:"terceiro", nome:"Matemática para Computação", cod:"ead05008", aulas:9, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTZ2VNU1NLY3NvZkU"};
+disciplina[9] = {periodo:"terceiro", nome:"Física para Computação", cod:"ead05015", aulas:26, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTOHVleklmRVM4OFE"};
+disciplina[10] = {periodo:"terceiro", nome:"Estrutura de Dados", cod:"ead05007", aulas:36, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTeVZZWVlpN1hRcHM"};
+disciplina[11] = {periodo:"terceiro", nome:"Programação com Interfaces Gráficas (Pascal)", cod:"ead05013", aulas:17, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTU09GNGNhdmFyMGc"};
+disciplina[12] = {periodo:"terceiro", nome:"Programação com Interfaces Gráficas (Python)", cod:"ead05030", aulas:13, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTU09GNGNhdmFyMGc"};
+disciplina[13] = {periodo:"quarto", nome:"Probabilidade e Estatistica", cod:"ead05012", aulas:12, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTWUVBSV80XzhlNUk"};
+disciplina[14] = {periodo:"quarto", nome:"Modelagem de Informação", cod:"ead05011", aulas:23, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTTWVxbEdiT1lUYkE"};
+disciplina[15] = {periodo:"quarto", nome:"Sistemas Operacionais", cod:"ead05016", aulas:12, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTQnZrSUlQdTgyWkU"};
+disciplina[16] = {periodo:"quarto", nome:"Banco de Dados", cod:"ead05014", aulas:29, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTU1RGakNiSzNYTTA"};
+disciplina[17] = {periodo:"quarto", nome:"Programação Orientada a Objetos", cod:"ead05021", aulas:13, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTbk83Q19XV2pNdHc"};
+disciplina[18] = {periodo:"quinto", nome:"Engenharia de Software", cod:"ead05019", aulas:18, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTNWIzWHFJVTFLUk0"};
+disciplina[19] = {periodo:"quinto", nome:"Redes de Computadores I", cod:"ead05020", aulas:14, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTSnVVYWlxREpWYUE"};
+disciplina[20] = {periodo:"quinto", nome:"Análise de Sistemas", cod:"ead05018", aulas:24, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTcW9Hc1d6dEMzRTQ"};
+disciplina[21] = {periodo:"quinto", nome:"Programação de Aplicações WEB", cod:"ead05017", aulas:14, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTeVFfT0U4NlYzZ2s"};
+disciplina[22] = {periodo:"sexto", nome:"Empreendedorismo e Ética", cod:"ead05024", aulas:14, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTT1JtbXhxTW16dDg"};
+disciplina[23] = {periodo:"sexto", nome:"Redes de Computadores II", cod:"ead05025", aulas:23, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTeG9OUjlVcXNkUTg"};
+disciplina[24] = {periodo:"sexto", nome:"Arquitetura de Sistemas", cod:"ead05022", aulas:21, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTQmYzWnoxam9OeFU"};
+disciplina[25] = {periodo:"sexto", nome:"Computação Gráfica", cod:"ead05023", aulas:19, drive:"https://drive.google.com/open?id=0B09LpkQ_E4zTZTY3cmRDMGNUeHc"};
 
 function escreveaulas(n) {   // Função para escrever as aulas da disciplina escolhida na página principal.
-    var conteudo = "<h4>" + disciplina[n].nome + "</h4>";
+    var conteudo = "<h4>" + disciplina[n].nome + "</h4> <h3>Aulas</h3>";
     for(var i = 1; i <= disciplina[n].aulas; i++) {
         if (i < 10) {
-            conteudo += "<a href='http://videoaula.rnp.br/v.php?f=/cederj/sistemas_comp/" + disciplina[n].cod + "/Aula_00" + i + "/Aula_00" + i +".xml' target='_blank'><p>Aula 0" + i + "</p></a>";
+            conteudo += "<a href='http://videoaula.rnp.br/v.php?f=/cederj/sistemas_comp/" + disciplina[n].cod + "/Aula_00" + i + "/Aula_00" + i +".xml' target='_blank'><p class='link_aula'>Aula 0" + i + "</p></a>";
         } else {
-            conteudo += "<a href='http://videoaula.rnp.br/v.php?f=/cederj/sistemas_comp/" + disciplina[n].cod + "/Aula_0" + i + "/Aula_0" + i +".xml'target='_blank'><p>Aula " + i + "</p></a>";
+            conteudo += "<a href='http://videoaula.rnp.br/v.php?f=/cederj/sistemas_comp/" + disciplina[n].cod + "/Aula_0" + i + "/Aula_0" + i +".xml'target='_blank'><p class='link_aula'>Aula " + i + "</p></a>";
         }
     }
+    conteudo += "<h3>Repositório</h3><a href='" + disciplina[n].drive + "' target='_blank'>" +
+        "<img src='_imagens/drive.png'>" +
+        "<p>Todo conteúdo compartilhado de " + disciplina[n].nome + " você pode encontrar aqui!</a>" +
+        "<p>Qualquer aluno com acesso ao email da UFF (@id.uff.br) tem acesso a nossa pasta.</p>" +
+        "<p>A pasta \"_Colaboração\" está liberada para o envio de material" +
+        "<p>Não tem? <a href='https://docs.google.com/document/d/1T2GfxduiXHQeX0FmJDLk9BFZs4oh8t2hf2g54q23xUY/edit?usp=sharing'>Informe-se aqui</a> </p></p><br>";
     document.getElementById("aulas").innerHTML = conteudo;
     document.getElementById("check").checked = false;
 }
